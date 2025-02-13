00e7e09c415628a422e140fecf66140fede4c452
YZhang223 <127790675+YZhang223@users.noreply.github.com>
2025-02-13 10:27:28 -0500

    Update TestSplayTree.cpp

--- lec04 Splay Tree/TestSplayTree.cpp
+++ lec04 Splay Tree/TestSplayTree.cpp
@@ -1,51 +1,33 @@
-    #include <iostream>
-    #include "SplayTree.h"
-    using namespace std;
+#include "SplayTree.h"
+#include <iostream>
 
-        // Test program
-    int main( )
-    {
-        SplayTree<int> t;
-        int NUMS = 30000;
-        const int GAP  =   37;
-        int i;
+using namespace std;
 
-        cout << "Checking... (no more output means success)" << endl;
+int main() {
+    SplayTree<int> tree;
 
-        for( i = GAP; i != 0; i = ( i + GAP ) % NUMS )
-            t.insert( i );
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(2);
+    tree.insert(4);
+    tree.insert(6);
+    tree.insert(8);
 
-        for( i = 1; i < NUMS; i+= 2 )
-            t.remove( i );
+    cout << "Pre-Order Traversal before splay: ";
+    tree.preOrderTraversal();
+    cout << endl;
+    cout << "Post-Order Traversal before splay: ";
+    tree.postOrderTraversal();
+    cout << endl;
+    if (tree.contains(3))
+        cout << "3 is in the tree\n"; 
 
-        if( NUMS < 40 )
-            t.printTree( );
-        if( t.findMin( ) != 2 || t.findMax( ) != NUMS - 2 )
-            cout << "FindMin or FindMax error!" << endl;
-
-        for( i = 2; i < NUMS; i+=2 )
-            if( !t.contains( i ) )
-                cout << "Find error1!" << endl;
-
-        for( i = 1; i < NUMS; i+=2 )
-        {
-            if( t.contains( i ) )
-                cout << "Find error2!" << endl;
-        }
-
-        SplayTree<int> t2;
-        t2 = t;
-
-        for( i = 2; i < NUMS; i+=2 )
-            if( !t2.contains( i ) )
-                cout << "Find error1!" << endl;
-
-        for( i = 1; i < NUMS; i+=2 )
-        {
-            if( t2.contains( i ) )
-                cout << "Find error2!" << endl;
-        }
-
-        cout << "Test completed." << endl;
-        return 0;
+    cout << "Pre-Order Traversal after splay: ";
+    tree.preOrderTraversal();
+    cout << endl;
+    cout << "Post-Order Traversal after splay: ";
+    tree.postOrderTraversal();
+    cout << endl;
+    return 0;
 }
